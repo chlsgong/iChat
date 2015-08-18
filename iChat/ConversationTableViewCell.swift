1fd13f6f1c47aa17cb4d6c7fcf137598c8ef7f98
Manasa Tipparam <Manasa@Manasas-MacBook-Pro.local>
2015-08-18 17:34:31 -0500

    storyboard stuff

---
+++ iChat/ConversationTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ConversationTableViewCell.swift
+//  iChat
+//
+//  Created by Manasa Tipparam on 8/18/15.
+//  Copyright (c) 2015 Charles Gong. All rights reserved.
+//
+
+import UIKit
+
+class ConversationTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
